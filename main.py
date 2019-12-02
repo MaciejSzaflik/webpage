48e2e2abcbdf73829cb723949fea4e1ffe3a9821
Maciej Szaflik <maciej.szaflik@gmail.com>
2019-12-02 22:27:19 +0100

    heroku name change

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+mainApp = Flask(__name__)
 
 @app.route("/")
 def hello_www():
     return "Hello World Wide Web!"
 
 if __name__ == '__main__':
-    app.run()
+    mainApp.run()
