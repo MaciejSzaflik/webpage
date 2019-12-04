7d68b237fe9b40cd1c31882e4790715eec5db958
Maciej Szaflik <maciej.szaflik@gmail.com>
2019-12-04 12:28:21 +0100

    remove previous

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from flask import Flask, url_for, render_template, request, Response
-from flask_static_compress import FlaskStaticCompress
 
 mainApp = Flask(__name__)
-mainApp.config['COMPRESSOR_DEBUG'] = mainApp.config.get('DEBUG')
-mainApp.config['COMPRESSOR_STATIC_PREFIX'] = 'static'
-mainApp.config['COMPRESSOR_OUTPUT_DIR'] = 'build'
-mainApp.static_folder = 'static'
-compress = FlaskStaticCompress(mainApp)
 
 @mainApp.route("/", methods=['GET'])
 def home():
