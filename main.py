0b4fb024777253c8e7b940d3c25ae1d35837d3e8
Maciej Szaflik <maciej.szaflik@gmail.com>
2019-12-04 12:17:55 +0100

    next tutorial step

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
-mainApp = Flask(__name__)
+from flask import Flask, url_for, render_template, request, Response
+from flask_static_compress import FlaskStaticCompress
 
-@mainApp.route("/")
-def hello_www():
-    return "Hello World Wide Web!"
+mainApp = Flask(__name__)
+mainApp.config['COMPRESSOR_DEBUG'] = mainApp.config.get('DEBUG')
+mainApp.config['COMPRESSOR_STATIC_PREFIX'] = 'static'
+mainApp.config['COMPRESSOR_OUTPUT_DIR'] = 'build'
+mainApp.static_folder = 'static'
+compress = FlaskStaticCompress(mainApp)
+
+@mainApp.route("/", methods=['GET'])
+def home():
+    return render_template('/index.html', title="Site")
 
 if __name__ == '__main__':
     mainApp.run()
